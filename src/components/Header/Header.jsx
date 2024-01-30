5daed2235bb2871c6c83b83b36af56cf635b9fb4
ratrikhanam000 <ratrikhanam000@gmail.com>
2024-01-30 15:28:53 +0600

    first commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+// import { Button } from "keep-react";
+// import React from "react";
 
-const Header = () => {
-    return (
-        <div>
-            <p>Header</p>
-        </div>
-    );
-};
+// const Header = () => {
+//   return (
+//     <div>
+//       <Button size="md" type="default">
+//         Default
+//       </Button>
+//       <Button size="md" type="primary">
+//         Primary
+//       </Button>
+//       <Button size="md" type="outlinePrimary">
+//         Outline Primary
+//       </Button>
+//     </div>
+//   );
+// };
 
 export default Header;
