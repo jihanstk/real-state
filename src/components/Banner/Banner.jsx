89f5af2f37e2be6798c365dd7af944f2434538bf
ratrikhanam000 <ratrikhanam000@gmail.com>
2024-02-02 12:18:12 +0600

    banner try to complete

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,72 +1,36 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "./banner.css";
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/autoplay";
 import "swiper/css/effect-fade";
-// import "swiper/css/navigation";
-// import "swiper/css/pagination";
-
-// import required modules
+import banner from "../../../assets/banner.jpg";
 import Image from "next/image";
-import { Autoplay, EffectFade } from "swiper/modules";
 const Banner = () => {
-  const houseImages = [
-    {
-      path: "https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=800",
-    },
-    {
-      path: "https://images.pexels.com/photos/2089698/pexels-photo-2089698.jpeg?auto=compress&cs=tinysrgb&w=800",
-    },
-    {
-      path: "https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg?auto=compress&cs=tinysrgb&w=800",
-    },
-    {
-      path: "https://images.pexels.com/photos/2635038/pexels-photo-2635038.jpeg?auto=compress&cs=tinysrgb&w=800",
-    },
-  ];
   return (
-    <div className="mt-20">
-      <Swiper
-        spaceBetween={30}
-        effect={"fade"}
-        loop
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        modules={[EffectFade, Autoplay]}
-        className="mySwiper"
-      >
-        {houseImages.map((image, i) => (
-          <SwiperSlide key={i} className=" w-full ">
-            <div className="relative mx-auto" style={{ width: "97%" }}>
-              <Image
-                width={1000}
-                height={1000}
-                style={{ width: "100%", height: "600px", objectFit: "cover" }}
-                className="mx-auto  rounded-3xl"
-                src={image.path}
-                alt="House Image"
-              />
-              <div className="absolute top-0 w-full h-full bg-black/50 rounded-3xl flex items-center ">
-                <div className=" banner-overlay  w-full md:ml-32 p-4 md:text-left text-center">
-                  <h1 className="md:text-5xl text-5xl  md:font-bold text-white">
-                    We will help you find your{" "}
-                    <span className="text-green-300">Wonderful</span> home
-                  </h1>
-                  <p className="text-slate-300 text-md mt-5 text-[20px]">
-                    A great plateform to buy, sell and rent your properties
-                    without any agent or commisions.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      {/* TODO Sell and search option with tab */}
+    <div>
+      <div className="relative mx-auto">
+        <Image
+          width={1000}
+          height={100}
+          style={{ width: "100%", height: "100vh", objectFit: "cover" }}
+          className="mx-auto"
+          src={banner}
+          alt="House Image"
+        />
+        <div className="absolute top-0 w-full h-full bg-black/50  flex items-center ">
+          <div className=" banner-overlay  w-full m-auto p-4 text-center">
+            <h1 className="md:text-5xl text-3xl  md:font-bold text-white">
+              Find Your Dream Home
+            </h1>
+            <p className="text-slate-300 text-md mt-5 text-[20px]">
+              We are recognized for exceeding client expectations and delivering
+              great results through dedication, ease of process, and
+              extraordinary services to our worldwide clients.
+            </p>
+          </div>
+        </div>
+      </div>
       <div></div>
     </div>
   );
